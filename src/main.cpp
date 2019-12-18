195dff18997aa26395b05aee58c39769eaf95465
reloadedd <reloadedd@protonmail.com>
2019-12-18 19:32:05 +0200

    Creat strucutra programului

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <graphics.h>
+#include <winbgim.h>
+#define WIDTH 1000
+#define HEIGHT 1000
+#define TITLE "Impartirea Careului"
+
+int main() {
+    // Declaratii variabile
+
+    // Initializeaza fereastra
+    initwindow(WIDTH, HEIGHT, TITLE);
+
+  
+    getch();
+    closegraph();
+
+    return 0;
+}
