7e2fc196d10b203bc4a381ab402db8abb0413a5d
reloadedd <reloadedd@protonmail.com>
2019-12-26 21:02:41 +0200

    Reformatat si reorganizat codul

--- include/functions.h
+++ include/functions.h
@@ -1,1 +1,81 @@
-*/ Fisierele de tip header vor fi puse in acest folder */
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <iostream>
+#include <cstring>
+
+#define LATIME_TABEL 400
+#define INALTIME_TABEL 400
+#define LINII 9
+#define COLOANE 9
+#define LIBER 0
+#define OCUPAT 2
+
+/* Informeaza compilatorul despre existenta acestor variabile, iar acesta va cauta
+ * declaratia acestor variabile in fisierele sursa ce includ aceasta librarie */
+extern unsigned int tabla[LINII][COLOANE];
+extern unsigned int nrBuline;
+
+using namespace std;
+
+
+/* Creeaza in mod vizual tabla de joc.
+ * Tabelul este creat utilizand linii.
+ */
+void creeaza_tabla()
+{
+    char numar_linie[2];
+
+    // deseneaza conturul tablei cu linii intrerupte, groase
+    setlinestyle(DASHED_LINE, 0x3333, THICK_WIDTH);
+    line(LATIME_TABEL, INALTIME_TABEL, 2 * LATIME_TABEL, INALTIME_TABEL);
+    line(LATIME_TABEL, INALTIME_TABEL, LATIME_TABEL, 2 * INALTIME_TABEL);
+    line(LATIME_TABEL, 2 * INALTIME_TABEL, 2 * LATIME_TABEL, 2 * INALTIME_TABEL);
+    line(2 * LATIME_TABEL, 2 * INALTIME_TABEL, 2 * LATIME_TABEL, INALTIME_TABEL);
+
+    // restabileste stilul liniilor la normal pentru a desena celulele tablei
+    setlinestyle(SOLID_LINE, 0x3333, NORM_WIDTH);
+    settextstyle(10, HORIZ_DIR, 4);
+
+    // numerotare pe verticala
+    for (int i = 410, c = 1; c < 9; i += 50, c++) {
+        itoa(c, numar_linie, 10);	// converteste variabila c in sir de caractere
+        outtextxy(370, i, numar_linie);
+    }
+
+    // numerotare pe orizontala
+    for (int i = 415, c = 1; c < 9; i += 50, c++) {
+        itoa(c, numar_linie, 10);
+        outtextxy(i, 365, numar_linie);
+    }
+
+    // deseneaza celulele tablei
+    for (int i = 50; i <= 400; i += 50)
+    {
+        line(LATIME_TABEL + i, INALTIME_TABEL, LATIME_TABEL + i, 2 * INALTIME_TABEL);
+        line(LATIME_TABEL, INALTIME_TABEL + i, 2 * LATIME_TABEL, INALTIME_TABEL + i);
+    }
+}
+
+
+/* Deseneaza o bulina intr-un patrat din tabla de joc */
+void deseneaza_bulina(int x, int y)
+{
+    unsigned int linie, coloana;
+
+    setcolor(DARKGRAY);
+    setfillstyle(SOLID_FILL, 8);
+    linie = (y - LATIME_TABEL) / 50 + 1;
+    coloana = (x - INALTIME_TABEL) / 50 + 1;
+
+    if (x > LATIME_TABEL&& x < 2 * LATIME_TABEL && y > INALTIME_TABEL&& y < 2 * INALTIME_TABEL)
+        if (tabla[linie][coloana] == LIBER)
+        {
+            nrBuline++;
+            tabla[linie][coloana] = OCUPAT;
+            circle(LATIME_TABEL + coloana * 50 - 25, INALTIME_TABEL + linie * 50 - 25, 20);
+            fillellipse(LATIME_TABEL + coloana * 50 - 25, INALTIME_TABEL + linie * 50 - 25, 20, 20);
+        }
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,83 @@
-#include <iostream>
 #include <graphics.h>
 #include <winbgim.h>
-#define WIDTH 1000
-#define HEIGHT 1000
-#define TITLE "Impartirea Careului"
+#include "../include/functions.h"   // Merge un folder inapoi, dupa care intra in folder-ul "include"
+
+#define LATIME 1200
+#define INALTIME 1000
+#define TITLU "Impartirea Careului"
+
+/* Declara variabilele marcate ca "extern" in fisierul header "functions.h" */
+unsigned int tabla[LINII][COLOANE];
+unsigned int nrBuline = 1;
+
 
 int main() {
-    // Declaratii variabile
+    int eroare, x, y, linie, coloana, patrat = 7, culoare = 1;
 
     // Initializeaza fereastra
-    initwindow(WIDTH, HEIGHT, TITLE);
+    initwindow(LATIME, INALTIME, TITLU);
 
-  
+    // Verificare erori si inchiderea imediata a programului in cazul in care acestea apar, informand utilizatorul
+    eroare = graphresult();
+
+    if (eroare != grOk) {    // a aparut o eroare
+        cout << "[Eroare]: " << grapherrormsg(eroare) << endl;
+        cout << "Apasati orice tasta pentru a opri programul..." << endl;
+
+        getch();
+        return -1;  // inchiderea programului si indicarea faptului ca procesul nu s-a terminat cu succes
+    }
+
+    // Deseneaza tabla de joc
+    creeaza_tabla();
+
+    while(nrBuline <= 8)
+    {
+        getmouseclick(WM_LBUTTONDOWN, x, y);
+        deseneaza_bulina(x, y);
+    }
+
+    while(true)
+    {
+        getmouseclick(WM_LBUTTONDOWN, x, y);
+        linie = (y - 400) / 50 + 1;
+        coloana = (x - 400) / 50 + 1;
+
+        if(tabla[linie][coloana] == OCUPAT)
+        {
+            setcolor(culoare);
+            culoare++;
+            for(int i = 1; i <= 4; i++) {
+                rectangle((x - 400) / 50 * 50 + i + 400, (y - 400) / 50 * 50 + i + 400,
+                          (x - 400) / 50 * 50 + 50 - i + 400, (y - 400) / 50 * 50 + 50 - i + 400);
+            }
+
+            while(patrat)
+            {
+                getmouseclick(WM_LBUTTONDOWN, x, y);
+                if(x > 400 && x < 800 && y > 400 && y < 800) {
+                    if(((((x - 400) / 50 + 2 == coloana) || ((x - 400) / 50 == coloana)) && (linie == (y - 400) / 50 + 1)) ||
+                       ((((y - 400) / 50 + 2 == linie) || ((y - 400) / 50 == linie)) && (coloana == (x - 400) / 50 + 1))) {
+                        if(tabla[(y - 400) / 50 + 1][(x - 400) / 50 + 1] == 0)
+                        {
+                            for(int i = 1; i <= 4; i++) {
+                                rectangle((x - 400) / 50 * 50 + i + 400, (y - 400) / 50 * 50 + i + 400,
+                                          (x - 400) / 50 * 50 + 50 - i + 400, (y - 400) / 50 * 50 + 50 - i + 400);
+                            }
+                            patrat--;
+                            tabla[linie][coloana] = 1;
+                            linie = (y - 400) / 50 + 1;
+                            coloana = (x - 400) / 50 + 1;
+                        }
+                    }
+                }
+            }
+        }
+        patrat = 7;
+    }
+
+    // "curatare"
     getch();
     closegraph();
-
     return 0;
 }
